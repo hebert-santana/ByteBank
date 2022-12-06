1b4668721b37b9235217573d51c4f084bbe4a359
Hebert Santana <102166830+hebert-santana@users.noreply.github.com>
2022-12-05 22:27:39 -0300

    add Conta Corrente

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,8 +2,6 @@ import {Cliente} from "./Cliente.js"
 
 export class ContaCorrente {
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
 
     set cliente(novoValor) {
         if (novoValor instanceof Cliente) {
@@ -21,9 +19,10 @@ export class ContaCorrente {
         return this._saldo;
     }
 
-    constructor(agencia, cliente) {
+    constructor(agencia, cliente, saldo, ) {
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js"
 
 const cliente1 = new Cliente("Hebert", 8999868621);
-const contaCorrenteCliente1 = new ContaCorrente(1001, cliente1);
-
-contaCorrenteCliente1._saldo = 100;
+const contaCorrenteCliente1 = new Conta(0, cliente1, 1001);
 contaCorrenteCliente1.depositar(500);
+contaCorrenteCliente1.sacar(100);
 
-const cliente2 = new Cliente("Maya", 77777777777);
-const contaCorrenteCliente2 = new ContaCorrente(1002, cliente2);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-contaCorrenteCliente2._saldo = 500;
-contaCorrenteCliente2.depositar(200);
-
-
-contaCorrenteCliente1.transferir(200, contaCorrenteCliente2);
-console.log(ContaCorrente.numeroDeContas);
-
-
-
-
-
-
-
-
-
+console.log(contaPoupanca);
 console.log(contaCorrenteCliente1);
-console.log(contaCorrenteCliente2);
-
